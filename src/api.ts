b4420c2d9f48e4cd502caadb6b76c17d8071674c
Neko <78627265+Nekoyre@users.noreply.github.com>
2021-08-13 21:42:53 -0500

    Add(Streamline requesting dapi)

--- src/api.ts
+++ src/api.ts
@@ -1,62 +1,81 @@
 import { Snowflake, APIMessage } from "discord-api-types";
-import fetch from "node-fetch"
 import MessageEmbed from "./structs/MessageEmbed";
 import { MessageOptions } from "./typings";
 import Utils from "./utils/Utils";
 
-const ROOT = "https://discord.com/api/v9";
 export default class APIRequestHandler {
+  public static async sendMessage(
+    channelID: Snowflake,
+    content: string | MessageEmbed | MessageOptions,
+    token: string
+  ) {
+    const opts = JSON.stringify(Utils.transformOptions(content));
 
-    public static async sendMessage(channelID: Snowflake, content: string | MessageEmbed | MessageOptions, token: string) {
-        const opts = JSON.stringify(Utils.transformOptions(content));
-    
-        const res = await fetch(`${ROOT}/channels/${channelID}/messages`, {
-            method: "POST",
-            body: opts,
-            headers: {
-                Authorization: `Bot ${token}`,
-                'Content-Type': 'application/json'
-            }
-        })
+    const res = await Utils.request(`/channels/${channelID}/messages`, "POST", {
+      body: opts,
+      headers: {
+        Authorization: `Bot ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
 
-        if (res.ok) {
-            return await res.json() as APIMessage;
-        } else {
-            return console.error("DiscordAPIError:", await res.json());
-        }
+    if (res.ok) {
+      return (await res.json()) as APIMessage;
+    } else {
+      return console.error("DiscordAPIError:", await res.json());
     }
+  }
 
-    public static async editMessage(channelID: Snowflake, messageID: Snowflake, content: string | MessageEmbed | MessageOptions, token: string) {
-        const opts = JSON.stringify(Utils.transformOptions(content));
+  public static async editMessage(
+    channelID: Snowflake,
+    messageID: Snowflake,
+    content: string | MessageEmbed | MessageOptions,
+    token: string
+  ) {
+    const opts = JSON.stringify(Utils.transformOptions(content));
 
-        const res = await fetch(`${ROOT}/channels/${channelID}/messages/${messageID}`, {
-            method: "PATCH",
-            body: opts,
-            headers: {
-                Authorization: `Bot ${token}`,
-                'Content-Type': 'application/json'
-            }
-        })
-        
-        if (res.ok) {
-            return await res.json() as APIMessage;
-        } else {
-            return console.error("DiscordAPIError:", await res.json());
-        }
+    const res = await Utils.request(
+      `/channels/${channelID}/messages/${messageID}`,
+      "PATCH",
+      {
+        body: opts,
+        headers: {
+          Authorization: `Bot ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (res.ok) {
+      return (await res.json()) as APIMessage;
+    } else {
+      return console.error("DiscordAPIError:", await res.json());
     }
+  }
 
-    public static async sendReaction(channelID: Snowflake, messageID: Snowflake, emoji: string, token: string) {
-        const res = await fetch(`${ROOT}/channels/${channelID}/messages/${messageID}/reactions/${encodeURIComponent(emoji)}/@me`, {
-            method: "PUT",
-            headers: {
-                Authorization: `Bot ${token}`
-            }
-        })
+  public static async sendReaction(
+    channelID: Snowflake,
+    messageID: Snowflake,
+    emoji: string,
+    token: string
+  ) {
+    const res = await Utils.request(
+      `/channels/${channelID}/messages/${messageID}/reactions/${encodeURIComponent(
+        emoji
+      )}/@me`,
+      "PUT",
+      {
+        headers: {
+          Authorization: `Bot ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-        if (res.ok) {
-            return true;
-        } else {
-            return console.error("DiscordAPIError:", await res.json());
-        }
+    if (res.ok) {
+      return true;
+    } else {
+      return console.error("DiscordAPIError:", await res.json());
     }
+  }
 }
--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -1,6 +1,12 @@
 import MessageEmbed from "../structs/MessageEmbed";
 import { MessageOptions } from "../typings";
-import { APIMessage, APIUser, APIGuildMember, Snowflake } from "discord-api-types";
+import {
+  APIMessage,
+  APIUser,
+  APIGuildMember,
+  Snowflake,
+} from "discord-api-types";
+import fetch from "node-fetch";
 import Message from "../structs/Message";
 import Guild from "../structs/Guild";
 import Channel from "../structs/Channel";
@@ -8,31 +14,75 @@ import User from "../structs/User";
 import GuildMember from "../structs/GuildMember";
 
 export default class Utils extends null {
-    
-    public static transformOptions(x: string | MessageEmbed | MessageOptions) {
-        let options: MessageOptions = {};
-        if (typeof x === "string") {
-            options.content = x;
-        } else if (x instanceof MessageEmbed) {
-            options.embeds = [x.toJson]
-        } else {
-            if (x.content) options.content = x.content;
-            if (x.embeds) options.embeds = x.embeds.map(e => e instanceof MessageEmbed && e.toJson);
-        }
+  public static async request(
+    endpoint: string,
+    method: string,
+    { body, headers }: { body?: any; headers: any }
+  ) {
+    return await fetch(`https://discord.com/api/v9${endpoint}`, {
+      method,
+      body,
+      headers,
+    });
+  }
 
-        return options;
+  public static transformOptions(x: string | MessageEmbed | MessageOptions) {
+    let options: MessageOptions = {};
+    if (typeof x === "string") {
+      options.content = x;
+    } else if (x instanceof MessageEmbed) {
+      options.embeds = [x.toJson];
+    } else {
+      if (x.content) options.content = x.content;
+      if (x.embeds)
+        options.embeds = x.embeds.map(
+          (e) => e instanceof MessageEmbed && e.toJson
+        );
     }
 
-    public static convertAPIMessage(message: APIMessage, channel: Channel, token: string) {
-        return new Message(message.id, channel.guild, channel, message.content, 
-            this.convertAPIUser(message.author), this.convertAPIMember(message.author, message.member, channel.guild), message.timestamp, message.edited_timestamp,token);
-    }
+    return options;
+  }
 
-    public static convertAPIUser(user: APIUser) {
-        return new User(user.id, user.username, user.discriminator, user.avatar, user.bot || false);
-    }
+  public static convertAPIMessage(
+    message: APIMessage,
+    channel: Channel,
+    token: string
+  ) {
+    return new Message(
+      message.id,
+      channel.guild,
+      channel,
+      message.content,
+      this.convertAPIUser(message.author),
+      this.convertAPIMember(message.author, message.member, channel.guild),
+      message.timestamp,
+      message.edited_timestamp,
+      token
+    );
+  }
 
-    public static convertAPIMember(user: User | APIUser, member: APIGuildMember, guild: Guild) {
-        return member ? new GuildMember(user instanceof User ? user : this.convertAPIUser(user), member.nick || null, member.roles, guild) : null;
-    }
+  public static convertAPIUser(user: APIUser) {
+    return new User(
+      user.id,
+      user.username,
+      user.discriminator,
+      user.avatar,
+      user.bot || false
+    );
+  }
+
+  public static convertAPIMember(
+    user: User | APIUser,
+    member: APIGuildMember,
+    guild: Guild
+  ) {
+    return member
+      ? new GuildMember(
+          user instanceof User ? user : this.convertAPIUser(user),
+          member.nick || null,
+          member.roles,
+          guild
+        )
+      : null;
+  }
 }
