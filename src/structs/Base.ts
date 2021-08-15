54fe4411653c5037441d212f7a1b5ae638d9d268
Neko <78627265+Nekoyre@users.noreply.github.com>
2021-08-15 18:33:40 -0500

    refactor(Js)
    Stop copying djs smh

--- src/structs/Base.ts
+++ src/structs/Base.ts
@@ -3,6 +3,6 @@ import Client from "./Client";
 export default class Base {
     client: Client
     constructor(client: Client) {
-        Object.defineProperty(this, 'client', { value: client });
+        this.client = client
     }
 }
