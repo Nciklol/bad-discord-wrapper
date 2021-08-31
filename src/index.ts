0a1340e07b5624b688333456eae6df3d56b994d2
Nciklol <itzthetester@gmail.com>
2021-08-30 20:28:25 -0500

    fix(Intents) & refactor(Support for raw event)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+// Structs
 import Client from "./structs/Client";
 import MessageEmbed from "./structs/MessageEmbed";
 import Message from "./structs/Message";
@@ -7,7 +8,27 @@ import Guild from "./structs/Guild";
 import GuildMember from "./structs/GuildMember";
 import Role from "./structs/Role";
 import User from "./structs/User";
+
+// Managers
+import GuildMemberManager from "./managers/GuildMemberManager";
+import WebSocketManager from "./managers/WebSocketManager";
+
+// Utils
 import Permissions from "./utils/Permissions";
 import Intents from "./utils/Intents";
 
-export {Client, MessageEmbed, Message, Channel, Base, Guild, GuildMember, Role, User, Permissions, Intents};
+export {
+    Client, 
+    MessageEmbed, 
+    Message, 
+    Channel, 
+    Base, 
+    Guild, 
+    GuildMember, 
+    Role, 
+    User, 
+    Permissions, 
+    Intents, 
+    GuildMemberManager, 
+    WebSocketManager
+};
--- src/structs/Client.ts
+++ src/structs/Client.ts
@@ -15,6 +15,7 @@ interface ClientEvents {
     guildMemberRemove: [guildID: Snowflake, user: User];
     debug: [str: string];
     guildMemberUpdate: [oldMember: GuildMember, newMember: GuildMember];
+    raw: [JSON]
 }
 
 export default class Client extends EventEmitter {
@@ -28,11 +29,11 @@ export default class Client extends EventEmitter {
 
     public on<K extends keyof ClientEvents>(event: K, listener: (...args: ClientEvents[K]) => Promise<void> | void): this {
         return super.on(event, listener);
-    }   
+    }
 
-    constructor(intents: number[]) {
+    constructor(intents: number | number[]) {
         super();
-        typeof intents === "number" ? this._intents = intents : this._intents = intents.reduce((x, y) => x += y);
+        typeof intents === "number" ? this._intents = intents : this._intents = intents.reduce((x, y) => x |= y);
     }
 
     public login(token?: string): void {
