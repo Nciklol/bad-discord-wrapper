7943804379a52c4dec389783d1590329f782f2b9
Nciklol <mr.awesome2006hi@gmail.com>
2021-08-30 20:33:42 -0500

    Revert "fix(Intents) & refactor(Support for raw event)"
    This reverts commit 0a1340e07b5624b688333456eae6df3d56b994d2.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-// Structs
 import Client from "./structs/Client";
 import MessageEmbed from "./structs/MessageEmbed";
 import Message from "./structs/Message";
@@ -8,27 +7,7 @@ import Guild from "./structs/Guild";
 import GuildMember from "./structs/GuildMember";
 import Role from "./structs/Role";
 import User from "./structs/User";
-
-// Managers
-import GuildMemberManager from "./managers/GuildMemberManager";
-import WebSocketManager from "./managers/WebSocketManager";
-
-// Utils
 import Permissions from "./utils/Permissions";
 import Intents from "./utils/Intents";
 
-export {
-    Client, 
-    MessageEmbed, 
-    Message, 
-    Channel, 
-    Base, 
-    Guild, 
-    GuildMember, 
-    Role, 
-    User, 
-    Permissions, 
-    Intents, 
-    GuildMemberManager, 
-    WebSocketManager
-};
+export {Client, MessageEmbed, Message, Channel, Base, Guild, GuildMember, Role, User, Permissions, Intents};
--- src/structs/Client.ts
+++ src/structs/Client.ts
@@ -15,7 +15,6 @@ interface ClientEvents {
     guildMemberRemove: [guildID: Snowflake, user: User];
     debug: [str: string];
     guildMemberUpdate: [oldMember: GuildMember, newMember: GuildMember];
-    raw: [JSON]
 }
 
 export default class Client extends EventEmitter {
@@ -29,11 +28,11 @@ export default class Client extends EventEmitter {
 
     public on<K extends keyof ClientEvents>(event: K, listener: (...args: ClientEvents[K]) => Promise<void> | void): this {
         return super.on(event, listener);
-    }
+    }   
 
-    constructor(intents: number | number[]) {
+    constructor(intents: number[]) {
         super();
-        typeof intents === "number" ? this._intents = intents : this._intents = intents.reduce((x, y) => x |= y);
+        typeof intents === "number" ? this._intents = intents : this._intents = intents.reduce((x, y) => x += y);
     }
 
     public login(token?: string): void {
